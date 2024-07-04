6938393f4bd0ef798b7b593dd95e543b014ebb9d
KoreaMoney <kimdowon1404@gmail.com>
2024-07-05 00:13:38 +0900

    first commit

---
+++ main.dart
@@ -0,0 +1,24 @@
+// main함수는 중요합니다.
+// void main() {
+//   print("hello world!");
+// }
+
+// 변수
+// var는 지역변수를 선언할 때 사용한다.
+// void main() {
+//   var name = "hello";
+//   name = 1; // type이 맞지 않아 문자열로 수정해야 한다.
+// }
+
+// void main() {
+//   String name = "hello"; // 명시적으로 변수의 타입 지정, class에서 변수나 속성을 선언할때 type를 지정한다.
+// }
+
+//  Dynamic type - 사용되는 것을 추천하지 않지만 때때로 유용하게 사용된다.
+// void main() {
+//   var name; // dynamically generated or dynamic name;
+//   name = "hello";
+//   name = 123;
+//   name = false; // type이 모두 가능하다.
+// }
+
